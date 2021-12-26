1eb9c66f8c499c45db4c5b4549264f0604dd93dc
Lucky <mukeshkushawaha>
2021-12-26 17:53:56 +0530

    hello jaanu fghjgh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div><h1>Naveenlandure</h1></div>
-      <div><h1>Naveenlandure</h1></div>
-    
+      <div>
+        <h1>Naveenlandure</h1>
+      </div>
+      <div>
+        <h1>Naveenlandure</h1>
+        <h1>hello my jaan</h1>
+      </div>
     </div>
   );
 }
